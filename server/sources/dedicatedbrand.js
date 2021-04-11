0964d1f5ad64cc1c81c3065a212d960f47cd373e
Quentin Oliveira Da Silva <da-silva@october.eu>
2021-04-11 15:02:45 +0200

    init

--- server/sources/dedicatedbrand.js
+++ server/sources/dedicatedbrand.js
@@ -16,6 +16,7 @@ const parse = data => {
         .text()
         .trim()
         .replace(/\s/g, ' ');
+        
       const price = parseInt(
         $(element)
           .find('.productList-price')
